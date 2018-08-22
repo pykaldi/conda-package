863f6ee6964551c8744a8881d8544809be62d939
Victor Martinez <vrmp00@gmail.com>
2018-08-21 19:44:39 -0700

    Updated meta to use git as source

--- pykaldi/build.sh
+++ pykaldi/build.sh
@@ -1,2 +1,2 @@
-cp libs/* $PREFIX/lib/
+cp $SRC_DIR/pykaldi/libs/* $PREFIX/lib/
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
