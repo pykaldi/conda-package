20c3f519cc80f27e1db09db6f59ab9172a5caa26
Victor Martinez <vrmp00@gmail.com>
2018-09-12 19:33:15 -0700

    Working

--- pykaldi/build.sh
+++ pykaldi/build.sh
@@ -6,24 +6,41 @@ export PYCLIF="/home/victor/miniconda3/bin/pyclif"
 export CLIF_MATCHER="/home/victor/miniconda3/clang/bin/clif-matcher"
 export LD_LIBRARY_PATH="/home/victor/miniconda3/lib/:${LD_LIBRARY_PATH}"
 
+# Create lib folder
+LIB_FOLDER="$SP_DIR/kaldi/lib"
+mkdir -p $LIB_FOLDER
+
+#####################################
+# Copy cuda
+#####################################
+DEPS_LIST=(
+	"/usr/local/cuda/lib64/libcublas.so"
+	"/usr/local/cuda/lib64/libcudart.so"
+	"/usr/local/cuda/lib64/libcurand.so"
+	"/usr/local/cuda/lib64/libcusparse.so"
+)
+for filepath in "${DEPS_LIST[@]}"; do
+	cp $filepath $LIB_FOLDER
+done
+	
+#####################################
 # Install kaldi locally
+#####################################
 cd "$SRC_DIR/tools"
 ./install_kaldi.sh
 
 #####################################
-# Update kaldi rpaths
+# Copy and update kaldi rpaths
 #####################################
-# Create lib folder
-mkdir -p $SP_DIR/kaldi/lib
 
-# Kaldi libraries
-cp $SRC_DIR/tools/kaldi/src/lib/*.so $SP_DIR/kaldi/lib
+# Kaldi libraries (copy files and links)
+cp $SRC_DIR/tools/kaldi/src/lib/*.so* $LIB_FOLDER
 
-# Openfst libraries (copy links and files)
-rsync --links $SRC_DIR/tools/kaldi/tools/openfst/lib/ $SP_DIR/kaldi/lib/
+# Openfst libraries (copy files and links)
+cp $SRC_DIR/tools/kaldi/tools/openfst/lib/*.so* $LIB_FOLDER
 
-# Update lib so files
-find $SP_DIR/kaldi/lib -maxdepth 1 -name "*.so" -type f | while read sofile; do
+# Update so files rpath to $ORIGIN and conda lib directory (relative to site-packages/kaldi/lib)
+find $SP_DIR/kaldi/lib -maxdepth 1 -name "*.so*" -type f | while read sofile; do
 	echo "Setting rpath of $sofile to \$ORIGIN, conda/lib"
 	patchelf --set-rpath '$ORIGIN:$ORIGIN/../../../..' $sofile
 done
@@ -33,22 +50,3 @@ done
 ##########################################################################
 cd "$SRC_DIR"
 $PYTHON setup.py install
-
-##########################################################################
-# Update pykaldi RPATHs to include $ORIGIN, pykaldi, kaldi/lib, and conda lib
-##########################################################################
-
-# Create an rpath string from a list of all pykaldi sub-packages
-# rpath="\$ORIGIN/.."
-# find $SP_DIR/kaldi -maxdepth 1 -type d -exec basename {} \; | while read pkg; do
-# 	if [[ $pkg -ne "__pycache__" ]] && [[ $pkg -ne "kaldi" ]]; then
-# 		rpath=rpath":\$ORIGIN/../$pkg"
-# 	fi
-# done
-
-# # Update so files
-# find $SP_DIR/kaldi -name "*.so" -type f | while read sofile; do
-# 	echo "Setting rpath of $sofile to \$ORIGIN, pykaldi rpath, kaldi/lib, conda/lib"
-# 	patchelf --set-rpath "$rpath:\$ORIGIN/../lib:\$ORIGIN/../../../.." $sofile
-# done
-
